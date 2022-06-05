99a8a748c56810b7a87f231f8cdaa31e130aa58c
faisalantu <faisalantu@gmail.com>
2022-06-05 13:45:07 +0600

    💪🏽 IndexPage of the dashboard

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BarChart from "./BarChart";
+
+
+
+const Index = () => {
+  return (
+    <div>
+      <div className="w-96">
+        <BarChart/>
+      </div>
+    </div>
+  );
+};
+
+export default Index;

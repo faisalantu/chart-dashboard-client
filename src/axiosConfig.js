59f000fe1b3c10400f4e8b3d7919a3a6a3e0e8b8
faisalantu <faisalantu@gmail.com>
2022-06-06 12:45:12 +0600

    🌐 Axios config created

---
+++ src/axiosConfig.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+console.log(process.env.REACT_APP_API_URL);
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
